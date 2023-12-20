f843a721d6755db1315f939775f339611669e1a9
markrubin920 <markrubin920@gmail.com>
2023-12-20 19:20:37 +0000

    Almost done

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,131 +3,79 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+# Create header of page
 st.markdown("<h1><center> Niche Coding Communities Call GitHub for Help</center></h1>", unsafe_allow_html=True)
 st.markdown("<h5><center>More Popular Languages Use it for Fun and Creativity</center></h5>", unsafe_allow_html=True)
-# """
 
+# Datapath
 csv_file_path = 'github_dataset2.csv'
 
+# Drop Null Languages
 df = pd.read_csv(csv_file_path)
-df = df.drop(df[df['language'] == ""].index)
+df = df.drop(df[df['Language'] == ""].index)
 
-df2 = pd.read_csv('github_dataset4.csv')
+# Get data for top left chart
+average_stars_by_language = df.groupby('Language').agg({'Stars Count': 'mean', 'Language Frequency': 'mean'}).reset_index()
+top_10_languages = average_stars_by_language.sort_values(by='Stars Count', ascending=False).head(10)
 
+# Get data for bottom left chart
+stars_by_freq = df.groupby('Language Frequency')['Stars Count'].mean()
+stars_by_freq = stars_by_freq.reset_index()
+
+# Get data for top right chart --> Language frequency by contributors and pull requests
+result = df.groupby('Language Frequency').agg({'Contributors': 'mean', 'Pull Requests': 'mean', 'Forks Count': 'mean'}).reset_index()
+
+# Additional dataframe for bottom right chart
+df2 = pd.read_csv('github_dataset3.csv')
+
+# Bottom right chart
 chart = alt.Chart(df2).mark_circle().encode(
-    x='forks_count',
-    y='pull_requests',
+    x='Forks Count',
+    y='Pull Requests',
     color='Frequency Category',
 ).interactive()
 
-average_watchers_by_language = df.groupby('language')['stars_count'].mean()
-
-# Reset the index to make 'primary_language' a column again
-average_watchers_by_language = average_watchers_by_language.reset_index()
-
-# Sort the DataFrame by the average watchers in descending order
-top_5_languages = average_watchers_by_language.sort_values(by='stars_count', ascending=False).head(10)
-
-# forks_by_language = df.groupby('language').agg({'Language Frequency': 'mean', 'forks_count': 'mean'}).reset_index()
-
-# Reset the index to make 'primary_language' a column again
-# forks_by_language = forks_by_language.reset_index()
-
-# Sort the DataFrame by the average watchers in descending order
-# top_5forks_languages = forks_by_language.sort_values(by='forks_count', ascending=False).head(7)
-
-
-stars_by_freq = df.groupby('Language Frequency')['stars_count'].mean()
-stars_by_freq = stars_by_freq.reset_index()
-
-# engagement_by_freq = df.groupby('Language Frequency')['pull_requests'].mean()['stars_count']
-
-# engagement_by_freq = engagement_by_freq.reset_index()
-
-
-result = df.groupby('Language Frequency').agg({'contributors': 'mean', 'pull_requests': 'mean', 'forks_count': 'mean'}).reset_index()
-# st.dataframe(result)
-
-# Create two columns
+# Two columns for layout
 col1, col2 = st.columns(2)
 
-# Place graphs or elements in each column
+# First column about less popular languages' repositories being used by other more for community help
 with col1:
-    st.markdown("<h2><center>Less Popular, More Useful</center></h2>", unsafe_allow_html=True)
-    st.bar_chart(data=top_5_languages, x='language', y='stars_count', color=None, width=0, height=0, use_container_width=False)
-    # Add your first graph here
-    st.markdown("<p><center>Less frequently used languages like Assembly, Vim Script and Vue receive most repository stars -- a proxy for repository usefulness</center></p>", unsafe_allow_html=True)
-    st.bar_chart(data=stars_by_freq, x='Language Frequency', y='stars_count', color=None, width=0, height=0, use_container_width=False)
+    # Title
+    st.markdown("<h2><center>Less Popular, More for Help</center></h2>", unsafe_allow_html=True)
+    # Top left chart layout
+    st.bar_chart(data=top_10_languages, x='Language', y='Stars Count', color="#FFA500", width=0, height=0, use_container_width=True)
+    # Caption
+    st.markdown("<p><center>Less frequently used languages like Assembly, Vim Script and Vue receive most repository stars -- a proxy for repository helpfulness</center></p>", unsafe_allow_html=True)
+    # Bottom left chart layout
+    st.bar_chart(data=stars_by_freq, x='Language Frequency', y='Stars Count', color="#7F00FF", width=0, height=0, use_container_width=True)
 
-
+# Second column about more popular languages' repositories being used by others more for others for inspitation
 with col2:
-    st.markdown("<h2><center>More Popular, More Community Action</center></h2>", unsafe_allow_html=True)
-    # Add your second graph here
+    # Title
+    st.markdown("<h2><center>More Popular, More for Inspiration</center></h2>", unsafe_allow_html=True)
+    # Top right chart layout
     st.vega_lite_chart(
         result,
         {
             "mark": {"type": "circle", "tooltip": True},
             "encoding": {
                 "x": {"field": "Language Frequency", "type": "quantitative"},
-                "y": {"field": "contributors", "type": "quantitative"},
-                # "size": {"field": "forks_count", "type": "quantitative"},
-                "color": {"field": "pull_requests", "type": "quantitative"},
+                "y": {"field": "Contributors", "type": "quantitative"},
+                "color": {"field": "Pull Requests", "type": "quantitative"},
             },
-        }
+        }, 
+        use_container_width=True
     )
-    st.markdown("<p><center>Repositories of more frequently used programming language have more forks, contributors and pull requests, indicating high engagement with the repository</center></p>", unsafe_allow_html=True)
+    # Caption
+    st.markdown("<p><center>Repositories of more popular programming language have more forks, contributors and pull requests, indicating high engagement</center></p>", unsafe_allow_html=True)
+    # Bottom right chart layout
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
-    # st.scatter_chart(data=df, x='Language Frequency', y='forks_count', color=None, width=0, height=0, use_container_width=False)
-    # st.scatter_chart(data=df, x='stars_count', y='pull_requests', color=None, size=None, width=0, height=0, use_container_width=True)
 
-
-
-# Use the `read_csv` function to read the CSV file into a DataFrame
+# Data table
 st.markdown("<p><center>Data Used for Tables</center></p>", unsafe_allow_html=True)
 st.dataframe(df)
 
+# Acknowledgements
 st.markdown("<p>Data source:</p>", unsafe_allow_html=True)
 st.markdown("<a>https://www.kaggle.com/datasets/nikhil25803/github-dataset/data</a>", unsafe_allow_html=True)
-
-
-
-
-
-# st.vega_lite_chart(
-#    df,
-#    {
-#        "mark": {"type": "circle", "tooltip": True},
-#        "encoding": {
-#            "x": {"field": "contributors", "type": "quantitative"},
-#            "y": {"field": "pull_requests", "type": "quantitative"},
-#         #    "size": {"field": "c", "type": "quantitative"},
-#            "color": {"field": "language", "type": "nominal"},
-#        },
-#    }, 
-# )
-
-# num_points = st.slider("Number of points in spiral", 1, 60000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-
-# df = pd.DataFrame({
-#     "x": y,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
+st.markdown("<p>Dashboard By: Mark Rubin</p>", unsafe_allow_html=True)
